edc5344f3b4ba050e9583106546dd8df94d249ae
bugidemaheshbabu <mababu9059@gmail.com>
2021-08-15 15:43:02 +0530

    Update Procfile

--- homelane_covid_19_3rd_wave_prev/settings.py
+++ homelane_covid_19_3rd_wave_prev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xl#2u&fc*o4lqv3_il+mxjgx1_0_tc00$=ygko32evi+%7u1k#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
